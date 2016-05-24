243bd75a55c7d8ebe9a6a4ee914f921930450bb5
Guilherme Pedrassa <guipedrassa@gmail.com>
2016-05-24 09:51:10 +0200

    drop and create hive table

--- src/main/scala/HiveHBasePoC.scala
+++ src/main/scala/HiveHBasePoC.scala
@@ -1,12 +1,59 @@
-import org.apache.spark.{SparkConf, SparkContext}
+import com.cloudera.spark.hbase.HBaseContext
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.client.{Put, Result}
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat
+import org.apache.spark.SparkContext
+import org.apache.hadoop.hbase.util.Bytes
 
 object HiveHBasePoC {
   def main(args: Array[String]) {
+
     val sc = new SparkContext()
 
     val sqlContext = new org.apache.spark.sql.hive.HiveContext(sc)
+
+    //create HIVE table and load data
     sqlContext.sql("DROP TABLE IF EXISTS employee")
     sqlContext.sql("CREATE TABLE IF NOT EXISTS employee(id INT, name STRING, age INT) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
+    sqlContext.sql("LOAD DATA LOCAL INPATH 'test.txt' OVERWRITE INTO TABLE employee")
+
+    // create DataFrame HIVE
+    val resultHive = sqlContext.sql("FROM employee SELECT *")
+
+    // HBase
+    val columnFamily = "Image Data"
+    val rdd = sc.parallelize(Array(
+      (Bytes.toBytes("1"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("1"), Bytes.toBytes("1")))),
+      (Bytes.toBytes("2"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("1"), Bytes.toBytes("2")))),
+      (Bytes.toBytes("3"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("1"), Bytes.toBytes("3")))),
+      (Bytes.toBytes("4"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("1"), Bytes.toBytes("4")))),
+      (Bytes.toBytes("5"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("1"), Bytes.toBytes("5"))))
+    ))
+
+    val conf = HBaseConfiguration.create()
+    val tableName = "employee_hbase"
+    conf.set("INPUT_TABLE", tableName)
+    conf.addResource(new Path("/etc/hbase/conf/core-site.xml"))
+    conf.addResource(new Path("/etc/hbase/conf/hbase-site.xml"))
+
+    val hbaseContext = new HBaseContext(sc, conf)
+
+    hbaseContext.bulkPut[(Array[Byte], Array[(Array[Byte], Array[Byte], Array[Byte])])](rdd,
+      tableName,
+      (putRecord) => {
+        val put = new Put(putRecord._1)
+        putRecord._2.foreach((putValue) => put.add(putValue._1, putValue._2, putValue._3))
+        put
+      },
+      true)
+
+
+    val rddResult = sc.newAPIHadoopRDD(conf, classOf[TableInputFormat], classOf[ImmutableBytesWritable], classOf[Result])
+
+    println("Hbase count "+rddResult.count)
+
 
   }
 }
